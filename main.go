e7d98975e78e239ea93b7e03ab7c09f521c6f79c
Gairik Sharma <88035292+GairikSharma@users.noreply.github.com>
2024-10-09 03:59:23 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	// "myproject/arithmetic"
+	"myproject/mypackage"
+)
+
+func main ()  {
+	fmt.Println("hello world I'm learning go...")
+	mypackage.Foo()
+	// res := arithmetic.Sum(10, 20)
+	// fmt.Println(res)
+}
